065e159f06eece3ce89c7150d0a7e647aa3ea7b5
Csanmart <csanmart2024@hotmail.com>
2025-06-16 20:08:49 -0500

    Configuramos el tailwind

--- frontend/my-react-app/src/App.jsx
+++ frontend/my-react-app/src/App.jsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 function App() {
   return(
     <section>
-        <h1>Hola mundo en vite</h1>
+        <h1 className="text-fuchsia-600">Hola mundo en vite</h1>
         <p>Vamos a crear componentes</p>
     </section>
   )
-}
+} 
 
 export default App
--- frontend/my-react-app/src/main.jsx
+++ frontend/my-react-app/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './css.css'
+import 'tailwindcss'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
